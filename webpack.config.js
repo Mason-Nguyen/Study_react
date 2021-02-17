88070fa8c30f5117e6f08d4d51af2815150c7c5f
Hau Nguyen <quanghau05@gmail.com>
2021-02-17 12:45:45 +0700

    0000 Config webpack watching with development mode

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,11 @@ module.exports = {
         path: path.join(__dirname, "dist", "assets"),
         filename: "bundle.js"
     },
+    //watch: true, // No need using watch: true, it automatically, and configured in package.json
+    watchOptions: {
+        ignored: /node_modules/ // No watch file .js in node_modules
+        // aggregateTimeout: 200 (ms) // - Add a delay before rebuilding once the first file changed. This allows webpack to aggregate any other changes made during this time period into one rebuild
+    },
     module: {
         rules: [
             {
